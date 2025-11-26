3061b446a0651b053852e47e37089f805fac732d
SATHISH B <sathishb2310@gmail.com>
2025-11-26 19:50:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-let currentAudio = null;
+// list of available sounds
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
 
-function playSound(soundPath) {
-    if (currentAudio) {
-        currentAudio.pause();
-        currentAudio.currentTime = 0;
-    }
+const buttonsContainer = document.getElementById("buttons");
 
-    currentAudio = new Audio(soundPath);
-    currentAudio.play();
-}
+// create a button for each sound
+sounds.forEach(sound => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
 
-function stopSound() {
-    if (currentAudio) {
-        currentAudio.pause();
-        currentAudio.currentTime = 0;
-    }
+  btn.addEventListener("click", () => {
+    stopAllSounds();
+    document.getElementById(sound).play();
+  });
+
+  buttonsContainer.appendChild(btn);
+
+  const audio = document.createElement("audio");
+  audio.id = sound;
+  audio.src = `./sounds/${sound}.mp3`;
+  document.body.appendChild(audio);
+});
+
+// stop button
+const stopBtn = document.createElement("button");
+stopBtn.classList.add("stop");
+stopBtn.innerText = "stop";
+
+stopBtn.addEventListener("click", stopAllSounds);
+
+buttonsContainer.appendChild(stopBtn);
+
+// function to stop all playing sounds
+function stopAllSounds() {
+  sounds.forEach(sound => {
+    const audio = document.getElementById(sound);
+    audio.pause();
+    audio.currentTime = 0;
+  });
 }
